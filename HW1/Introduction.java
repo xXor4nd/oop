2f7958cd3fe57e9488916a5f60e5f915fa75b068
xXor4nd <vanducdo2705@gmail.com>
2025-09-20 16:30:44 +0700

    Pushing HW1

---
+++ HW1/Introduction.java
@@ -0,0 +1,10 @@
+public class Introduction {
+    public static void main(String[] args) {
+        System.out.println("Họ tên: Đỗ Văn Đức\tMSV: 24021410\tLớp: K69I-CS5\tUsername: xXor4nd\tEmail: vanducdo2705@gmail.com");
+        for (int i = 9; i >= 1; --i) {
+            System.out.println(i + " bottles of beer on the wall, " + i + " bottles of beer.");
+            System.out.println("Take one down, pass it around,");
+        }
+        System.out.println("No more bottles of beer on the wall.");
+    }
+}
